104b06088d70ecf5307fbfa4a7b6979b3a54462d
nathan-truong-TAMU <nathanthetruong.235@tamu.edu>
2024-10-04 09:40:49 -0500

    Added all columns to their params as well as added admin access through Google OAuth

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -75,6 +75,6 @@ class EventsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def event_params
-    params.require(:event).permit(:link, :name, :start_date, :end_date, :points, :description)
+    params.require(:event).permit(:link, :name, :start_date, :end_date, :points, :description, :category, :location)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,7 +34,9 @@ class SessionsController < ApplicationController
   def create
     @events = Event.all
     @links = Link.all
-    if params[:password] == 'TxAMHeat#2k13'
+
+    member = Member.find_by(email: params[:email])
+    if params[:password] == 'TxAMHeat#2k13'|| member&.position == 'Admin'
       session[:authenticated] = true
       session[:view_mode] = 'admin'
       redirect_to session[:previous_url] || root_path, notice: 'Successfully authenticated.'
